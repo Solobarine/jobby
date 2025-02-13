a3b429076f0a02a05bd986cce8c03a1c36da7253
Solly <solobarine@gmail.com>
2025-02-13 11:46:11 +0100

    fix params issues

--- src/app/company/jobs/[id]/applications/page.tsx
+++ src/app/company/jobs/[id]/applications/page.tsx
@@ -5,9 +5,9 @@ import { notFound } from "next/navigation";
 export default async function ManageApplicationsPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const jobId = Number.parseInt(params.id);
+  const jobId = parseInt((await params).id);
   const [job, applications] = await Promise.all([
     getJobById(jobId),
     getApplicationsByJobId(jobId),
